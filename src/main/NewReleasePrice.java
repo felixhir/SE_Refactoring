ec2afa5bdb0ecb6c1e0ff20927bda5610301161c
felixhir <felix.hirschel@outlook.de>
2021-05-08 10:46:07 +0200

    moved getCharge() from Movie.java to the subclasses of Price.java to make switch more maintainable

--- src/main/NewReleasePrice.java
+++ src/main/NewReleasePrice.java
@@ -3,4 +3,10 @@ public class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    double getCharge(int daysRented){
+        return daysRented * 3;
+    }
+
 }
--- src/main/Price.java
+++ src/main/Price.java
@@ -1,3 +1,4 @@
 abstract public class Price {
     abstract int getPriceCode();
+    abstract double getCharge(int daysRented);
 }
