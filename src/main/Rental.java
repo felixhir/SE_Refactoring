c9348dee801054dcd6e99fdcddc55b8e8482e913
felixhir <felix.hirschel@outlook.de>
2021-05-08 10:26:44 +0200

    moved getCharge() from Rental.java to Movie.java

--- src/main/Rental.java
+++ src/main/Rental.java
@@ -1,6 +1,8 @@
 class Rental {
+
     private Movie movie;
     private int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
@@ -8,28 +10,13 @@ class Rental {
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
     }
 
     public double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
