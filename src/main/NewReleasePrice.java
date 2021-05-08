991ebfef1bbe4248b1dbb34bfcfb3e93c650c07c
felixhir <felix.hirschel@outlook.de>
2021-05-08 10:56:52 +0200

    Extracted getter for FrequentRenterPoints from Rental.java

--- src/main/NewReleasePrice.java
+++ src/main/NewReleasePrice.java
@@ -4,9 +4,12 @@ public class NewReleasePrice extends Price {
         return Movie.NEW_RELEASE;
     }
 
-    @Override
     double getCharge(int daysRented){
         return daysRented * 3;
     }
 
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
 }
--- src/main/Rental.java
+++ src/main/Rental.java
@@ -20,8 +20,6 @@ class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
